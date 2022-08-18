05a4415a80682b10b840ca670722fb42ea90fb8d
Luis Gustavo Cardoso Zanetti Pereira <luisgustavoczp@gmail.com>
2022-08-18 22:15:38 +0000

    update server path windows/linux

--- server/src/index.js
+++ server/src/index.js
@@ -4,10 +4,11 @@ const port = 8000;
 const app = express();
 
 const path = __dirname;
-const serverPath = path.slice(0, path.lastIndexOf('\\'));
-const rootPath = serverPath.slice(0, serverPath.lastIndexOf('\\'));
+const isCounterBar = path.includes('\\');
+const serverPath = path.slice(0, isCounterBar ? path.lastIndexOf('\\') : path.lastIndexOf('/'));
+const rootPath = serverPath.slice(0, isCounterBar ? serverPath.lastIndexOf('\\') : serverPath.lastIndexOf('/'));
 const gamePath = `${rootPath}/public/game`;
-
+console.log(gamePath);
 
 app.use('/scripts', express.static(`${gamePath}/dist`));
 app.use('/data', express.static(`${gamePath}/data`));
