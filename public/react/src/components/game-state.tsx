9a6cf467cea92d4b693ff604c4b500533a549510
Luis Gustavo Zanetti <49000286+LuisGustavoCZP@users.noreply.github.com>
2022-09-04 20:44:54 -0300

    front: update gamestate props

--- public/react/src/components/game-state.tsx
+++ public/react/src/components/game-state.tsx
@@ -2,7 +2,9 @@ import { InventoryState } from "./inventory-state";
 import { PlayersState } from "./players-state";
 import { SpecsUser } from "./specsuser";
 import {ModalPergunta} from './modalpergunta'
-export function GameState (props : any)
+import { IGameProps } from "./game-room";
+
+export function GameState (props : IGameProps)
 {
     return (
     <section className="flex flex-col h-full w-full min-w-[200px] max-w-[400px] gap-4">
