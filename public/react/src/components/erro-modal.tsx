2b3c4c72318b06bd0bfbb58a1de9937055ec6aba
Mark <marcosrafaelp.camargo@gmail.com>
2022-09-21 18:50:31 -0300

    done

--- public/react/src/components/erro-modal.tsx
+++ public/react/src/components/erro-modal.tsx
@@ -3,18 +3,19 @@ import { useEffect, useState, ReactNode } from "react";
 type modalErrorType = {
     message:string,
     state:string,
+    slideState:string,
     closeModal:() => void
 }
 
-export function ModalError({message,state,closeModal}:modalErrorType){
+export function ModalError({message,state, slideState, closeModal}:modalErrorType){
     
     return( <div onClick={()=>{closeModal()}} className={"bg-[#00000099] flex-col-reverse w-screen h-screen fixed m-0 "+state}>
-        <div className="h-[20%] w-1/6 bg-[rgb(46,46,46)] relative flex flex-col content-center items-center transform transition-all self-end justify-self-end place-self-end m-4">
-            <div className="flex justify-between px-3 items-center w-full bg-[#dadada] text-[#0f0f0f]">
+        <div className={"h-2/6 w-2/6 bg-[rgb(46,46,46)] relative flex flex-col transform transition-all self-end justify-self-end place-self-end my-4 mx-4 "+slideState}>
+            <div id="test" className="flex justify-between px-3 items-center w-full bg-[#dadada] text-[#0f0f0f]">
                 <p>Aviso!</p>
                 <button>X</button>
             </div>
-            <p className="self-center place-self-center py-6">{`${message}`}</p>
+            <p className="p-4">{`${message}`}</p>
         </div>
     </div>)
 }
--- public/react/src/pages/login.tsx
+++ public/react/src/pages/login.tsx
@@ -7,12 +7,16 @@ import loginbg from '../assets/sprites/loginbg.png'
 import { APIResponse } from '../models';
 import { ModalError } from '../components/erro-modal';
 export function Login(){
+    const [slideState, setSlideState] = useState('translate-x-full');
     const [modalState,setModalState]=useState('hidden')
     const [modalMessage,setModalMessage] = useState(' ')
     const {server} = useContext(GlobalContext);
     const navigate = useNavigate()
     function closeModal(){
-        setModalState('hidden')
+        setSlideState('translate-x-full');
+        setTimeout(()=>{
+            setModalState('hidden')
+        }, 500);
     }
 
     async function loginPlayer(){
@@ -21,13 +25,18 @@ export function Login(){
             "name":(document.getElementById('login-user-input')as HTMLInputElement).value,
             "password":(document.getElementById('login-user-password')as HTMLInputElement).value
         }
-        const resposta : APIResponse = await fetch(`https://${server}/users/login`, {
+
+        //alterei o protocolo https=>http.
+        const resposta : APIResponse = await fetch(`http://${server}/users/login`, {
             method: "POST",
             body: JSON.stringify(body),
             headers: {"Content-type": "application/json;charset=UTF-8"}
         }).then(resp => resp.json()).catch(err => {console.log(err); return {error:err}});
         console.log(resposta)
         if(resposta?.error){
+            setTimeout(()=>{
+                setSlideState('translate-x-0');
+            }, 500);
             setModalState('flex')
             setModalMessage(resposta.error)
             return
@@ -53,7 +62,7 @@ export function Login(){
                 <button onClick={loginPlayer} className='bg-[#1C1C1C] border-2 border-black leading-[35px] text-[25px] text-white cursor-pointer hover:text-[#7A7A7A] hover:bg-white hover:border-white transition-all' type="button">Entrar</button>
             </form>
         </div>
-        <ModalError message={modalMessage} state={modalState} closeModal={closeModal}/>
+        <ModalError message={modalMessage} state={modalState} slideState={slideState} closeModal={closeModal}/>
     
     </div>)
 }
