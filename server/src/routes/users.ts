5bf3f13c838d0c2309b8d884a489d98630471457
Luis Gustavo Zanetti <49000286+LuisGustavoCZP@users.noreply.github.com>
2022-09-23 15:28:23 -0300

    bugfix logout

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -7,8 +7,8 @@ import { tokenHandler } from "../middlewares/token";
 const router = Router();
 
 router.post('/register', HandlerRegister.init);
-router.post('/logout', LoginHandler.finish);
 router.post('/login', LoginHandler.init);
+router.get('/logout', tokenHandler, LoginHandler.finish);
 router.get('/', tokenHandler, getUserHandler);
 
 export default router;
