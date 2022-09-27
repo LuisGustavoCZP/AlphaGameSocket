a7a2678be66e412317155c99156b2cf7b98a1dfe
FelipeTRpo <felipetrpoke@hotmail.com>
2022-09-27 15:53:01 -0300

    tutorial modal add

--- public/react/src/components/userinfo.tsx
+++ public/react/src/components/userinfo.tsx
@@ -5,14 +5,25 @@ import { MatchHistoric } from "./match-historic";
 import { PlayerContext } from "../contexts";
 import { logOut } from "../utils/logout";
 import configs from "../utils/config";
+import { TutorialModal } from "./tutorial-modal";
 export function UserInfo(){
     const {getUserData } = useContext(PlayerContext);
-
+    const [tutorial,handleTutorial] = useState(<></>)
+    function closeModal(){
+        handleTutorial(<></>)
+    }
+    function openTutorial(){
+        handleTutorial(<TutorialModal closeModal={closeModal} />)
+    }
     console.log(getUserData)
     return <div className="w-1/5 bg-[#232323] h-full flex flex-col justify-between border-solid border-black border-r-2">
         <SpecsUser player={getUserData}/>
         <MatchHistoric/>
         <Ranking/>
-        <button className="justify-self-end self-end" onClick={()=>{logOut(configs.server)}}>LogOut</button>
+        {tutorial}
+        <div className="justify-self-end self-end flex justify-between w-full">
+            <button className="justify-self-end self-end" onClick={()=>{openTutorial()}}>Tutorial</button>
+            <button className="justify-self-end self-end" onClick={()=>{logOut(configs.server)}}>LogOut</button>
+        </div>
     </div>
 }
