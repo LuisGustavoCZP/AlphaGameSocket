26d709b24483a76cea06bb6d3ffa528031f74ef6
Luis Gustavo Zanetti <49000286+LuisGustavoCZP@users.noreply.github.com>
2022-09-28 14:22:13 -0300

    update speed

--- game/src/controllers/events/event-safe.ts
+++ game/src/controllers/events/event-safe.ts
@@ -8,7 +8,7 @@ import { GameEvent } from "./event";
 export class GameEventSafe extends GameEvent
 {
     eventID = -4;
-    timeout = 5000*(1/gameSpeed);
+    timeout = 10000*(1/gameSpeed);
 
     itemID : number;
     usableItems : Item[];
